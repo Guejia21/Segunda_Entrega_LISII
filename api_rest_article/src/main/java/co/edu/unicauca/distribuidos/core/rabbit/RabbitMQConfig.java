dca7ed27de16d190711a443107b6e6009debf966
Guejia21 <jonathan.guejia@unicauca.edu.co>
2024-10-29 12:22:53 -0500

    Some configuration

--- api_rest_article/src/main/java/co/edu/unicauca/distribuidos/core/rabbit/RabbitMQConfig.java
+++ api_rest_article/src/main/java/co/edu/unicauca/distribuidos/core/rabbit/RabbitMQConfig.java
@@ -9,8 +9,7 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
 
 @Configuration
 public class RabbitMQConfig {
